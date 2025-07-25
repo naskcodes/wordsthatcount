27ac94a1c4895b1c1a18ecb383e5b84df6c380ea
Roberta Nascimento <roberta.nask@gmail.com>
2025-07-25 16:11:16 -0300

    feat: recebimento e exibição de usuário, quantidade de palavras e data

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+Console.WriteLine("Digite seu usuário: ");
+string username = Console.ReadLine()!;
+Console.WriteLine("Digite a quantidade de palavas: ");
+int palavras = int.Parse(Console.ReadLine()!);
+string data = DateTime.Now.ToString();
+Console.WriteLine($"Usuário: {username}\nPalavras: {palavras}\nData: {data}");
