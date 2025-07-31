2e719cf99bdd86e40faf81665914307a353a3b9b
Roberta Nascimento <roberta.nask@gmail.com>
2025-07-31 15:01:42 -0300

    fix: devido a problemas de aprendizagem e problemas de conexao, essa desenvolvedora achou mais produtivo para o sistema retornar ao projeto básico, ampliando de acordo com a ampliação dos conhecimentos.

---
+++ 0.1/Back/Back/Controllers/RegistroController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Win32;
+using Back.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace wordsthatcount.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RegistroController : ControllerBase
+{
+    private readonly DbContextWordsThatCount _wtcdbcontext;
+
+    public RegistroController(DbContextWordsThatCount wtcdbcontext)
+    {
+        _wtcdbcontext = wtcdbcontext;
+    }
+
+    [HttpGet]
+    public IActionResult Registros()
+    {
+        var retorno = _wtcdbcontext.Registros.AsNoTracking().OrderBy(p => p.dataRegistro).ToList();
+        return Ok(retorno);
+    }
+
+    [HttpPost]
+    public IActionResult RegistraRegistro([FromBody] Registro registro)
+    {
+        _wtcdbcontext.Registros.Add(registro);
+        _wtcdbcontext.SaveChanges();
+        return Ok(registro);
+    }
+}
---
+++ Back/Registro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace back
+{
+    class Registro
+    {
+        public int Id { get; set; }
+        public int palavras { get; set; }
+        public string usuario { get; set; }
+        public DateTime dataRegistro { get; set; }
+
+        public void ExibirRegistro()
+        {
+            Console.WriteLine($"Usuário: {usuario}");
+            Console.WriteLine($"Data: {dataRegistro}");
+            Console.WriteLine($"Palavras: {palavras}");
+        }
+    }
+}
